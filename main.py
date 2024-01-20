50afec426a440b4a21b572b19d958c7905e937b6
paradox4280 <zmf5c2fi@pm.me>
2024-01-20 05:49:33 +0430

    Initial commit

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
-import urllib.parse
-import urllib.request
+import requests
 
 from dotenv import dotenv_values
-from fastapi import FastAPI, Body, status
-
-config = {**dotenv_values('.env')}
-
-app = FastAPI()
+from dataclasses import dataclass
+from bottle import Bottle, request, run
 
 
-@app.get('/ping', status_code=status.HTTP_200_OK)
-async def ping() -> dict[str, str]:
+@dataclass
+class Config:
+    CHAT_ID: str | None
+    BOT_TOKEN: str | None
+
+
+config = Config(**dotenv_values('.env'))
+
+app = Bottle()
+
+
+@app.route('/ping', method='GET')
+def ping():
     return {'message': 'ok'}
 
 
-@app.post('/notify', status_code=status.HTTP_200_OK)
-async def notify(message: str = Body(...)) -> dict[str, str] | None:
-    if len(message) > 2048:
-        return {'error': 'message length greater then 2048'}
-    params = urllib.parse.urlencode({'chat_id': config['CHAT_ID'],'text': message})
-    url = f'https://api.telegram.org/bot{config["BOT_TOKEN"]}/sendMessage?{params}'
-    with urllib.request.urlopen(url) as f:
-        print(f.read().decode('ascii'))
+@app.route('/myip', method='GET')
+def show_ip() -> dict[str, str]:
+    return {'my_ip': request.environ.get('REMOTE_ADDR')}
+
+
+@app.route('/notify', method='POST')
+def notify() -> None:
+    message = request.body.read().decode('ascii')
+    params = {'chat_id': config.CHAT_ID,'text': message}
+    url = f'https://api.telegram.org/bot{config.BOT_TOKEN}/sendMessage?{params}'
+    requests.get(url, data=params)
+
+
+if __name__ == '__main__':
+    run(app, host='localhost', debug=False)
